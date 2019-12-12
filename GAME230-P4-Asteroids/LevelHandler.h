85ce79c83ee2a1592f22d898e0891017c8ae8f62
Jason Tsai <jasonexe5@gmail.com>
2019-12-12 15:22:54 -0800

    feat: Add bucket grid

--- GAME230-P4-Asteroids/LevelHandler.h
+++ GAME230-P4-Asteroids/LevelHandler.h
@@ -6,9 +6,18 @@
 
 class LevelHandler {
 public:
+	int COLUMNS;
+	int ROWS;
+	float BUCKET_WIDTH;
+	float BUCKET_HEIGHT;
+	std::vector<std::vector<GameObject*>> gridBuckets;
+
 	LevelHandler();
 	void update();
 	void render(sf::RenderWindow& window);
 	void buildLevel(int level);
 	void checkCollision();
+	void clearBucket();
+	void addToBuckets(GameObject* obj);
+	int getGrid(sf::Vector2f pos);
 };
--- GAME230-P4-Asteroids/levelHandler.cpp
+++ GAME230-P4-Asteroids/levelHandler.cpp
@@ -7,6 +7,15 @@
 
 LevelHandler::LevelHandler()
 {
+	this->COLUMNS = 2;
+	this->ROWS = 2;
+	this->BUCKET_WIDTH = WINDOW_WIDTH / this->COLUMNS;
+	this->BUCKET_HEIGHT = WINDOW_HEIGHT / this->ROWS;
+
+	for (int i = 0; i < this->COLUMNS * this->ROWS; i++) {
+		std::vector<GameObject*> vObj;
+		this->gridBuckets.push_back(vObj);
+	}
 }
 
 void LevelHandler::update()
@@ -22,6 +31,7 @@ void LevelHandler::update()
 	for (int i = 0; i < asteroids.size(); i++) {
 		asteroids.at(i)->update();
 	}
+	this->checkCollision();
 }
 
 void LevelHandler::render(sf::RenderWindow& window)
@@ -44,5 +54,51 @@ void LevelHandler::buildLevel(int level)
 
 void LevelHandler::checkCollision()
 {
-	
+	this->clearBucket();
+	for (int i = 0; i < bullets.size(); i++) {
+		addToBuckets(bullets.at(i));
+	}
+	for (int i = 0; i < asteroids.size(); i++) {
+		addToBuckets(asteroids.at(i));
+	}
+	this->addToBuckets(player);
+}
+
+void LevelHandler::clearBucket()
+{
+	for (int i = 0; i < this->COLUMNS * this->ROWS; i++) {
+		this->gridBuckets.at(i).clear();
+	}
+}
+
+void LevelHandler::addToBuckets(GameObject* obj)
+{
+	std::vector<int> atBuckets;
+	atBuckets.push_back(this->getGrid(sf::Vector2f({ obj->position.x - obj->radius,obj->position.y - obj->radius })));
+	atBuckets.push_back(this->getGrid(sf::Vector2f({ obj->position.x + obj->radius,obj->position.y - obj->radius })));
+	atBuckets.push_back(this->getGrid(sf::Vector2f({ obj->position.x - obj->radius,obj->position.y + obj->radius })));
+	atBuckets.push_back(this->getGrid(sf::Vector2f({ obj->position.x + obj->radius,obj->position.y + obj->radius })));
+	std::sort(atBuckets.begin(), atBuckets.end());
+	atBuckets.erase(std::unique(atBuckets.begin(), atBuckets.end()), atBuckets.end());
+
+	for (int i = 0; i < atBuckets.size(); i++) {
+		gridBuckets[atBuckets.at(i)].push_back(obj);
+	}
+	for (int i = 0; i < gridBuckets.size(); i++) {
+		std::cout << i << ": ";
+		std::cout << gridBuckets.at(i).size() << " ";
+	}
+	std::cout << std::endl;
+}
+
+int LevelHandler::getGrid(sf::Vector2f pos)
+{
+	int col = int(pos.x / this->BUCKET_WIDTH);
+	if (col < 0) col = 0;
+	if (col >= this->COLUMNS) col = this->COLUMNS - 1;
+	int row = int(pos.y / this->BUCKET_HEIGHT);
+	if (row < 0) row = 0;
+	if (row >= this->ROWS) row = this->ROWS - 1;
+
+	return row * this->COLUMNS + col;
 }
